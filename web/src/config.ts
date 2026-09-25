3dd74d7534587dd4e08c54eb4f3e851d622a79d8
oblachka81-cloud <oblachka81@gmail.com>
2026-09-26 00:29:38 +0800

    Update FACTORY_ADDRESS for mainnet configuration

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,5 +1,5 @@
 export const NETWORK: 'testnet' | 'mainnet' = 'mainnet';
-export const FACTORY_ADDRESS = 'EQBIykBG5TFnaeEhGhclWj_jKexv5OyAnXzBgEixwlTQKWTB'; 
+export const FACTORY_ADDRESS = 'EQCMcVc-pvcr9J5U7pFqDfO7YoSoi8lg_MJpS-0rB4PBnjyI'; 
 export const API_URL = 'https://neuronvesting.bothost.tech';
 export const MANIFEST_URL = 'https://oblachka81-cloud.github.io/neuron-vesting/tonconnect-manifest.json';
 export const IS_TEST = FACTORY_ADDRESS.startsWith('kQ') || FACTORY_ADDRESS.startsWith('0Q');
